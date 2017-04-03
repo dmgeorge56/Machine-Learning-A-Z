786787a21d29deb74e09721782d57527d9d8a137
Dana George <dana@Danas-MacBook-Pro.local>
2017-04-02 20:23:24 -0600

    added code to fit polynomial regression to true data

--- polynomial_code.py
+++ polynomial_code.py
@@ -35,4 +35,52 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)
 """
+##fitting linear regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(x,y)
+
+##fitting polynomial regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=4) ##or 2 for squared, pick by the fit of the model on the plot
+x_poly = poly_reg.fit_transform(x)
+
+##new linear regression object to use poly_reg object in linear reg model
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly, y)
+
+##visualizing the linear regression results 
+plt.scatter(x,y, color='red')
+plt.plot(x,lin_reg.predict(x), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('position level')
+plt.ylabel('salary')
+plt.show
+
+##visualizing the polynomial regression results
+x_grid = np.arange(min(x), max(x), 0.1) ##to create curved line
+x_grid = x_grid.reshape((len(x_grid),1)) ##to create curved line
+
+plt.scatter(x, y, color='red')
+plt.plot(x_grid, lin_reg2.predict(poly_reg.fit_transform(x_grid)), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('position level')
+plt.ylabel('salary')
+plt.show
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
