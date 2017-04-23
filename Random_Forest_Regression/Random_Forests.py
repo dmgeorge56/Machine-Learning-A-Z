5d4d97ae0a42778f2d417e75758d0ed257549006
Dana George <dana@Danas-MacBook-Pro.local>
2017-04-23 13:46:22 -0600

    added random forest folder

---
+++ Random_Forest_Regression/Random_Forests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 15:27:40 2017
+
+@author: dana
+"""
+
+##Random Forests Intuition Notes
+##applied to regression trees
+##ensemble learning:
+    ##when you take multiple algorithms and put 
+    ##them together to make something more powerful
+    ##than the original
+##get multiple predictions and take the average
+##of the predictions over all of the trees
+##reminds him of:
+        ##take a guess how many m&ms are in the jar
+        ##best way to beat that game is
+        ##get a pen and paper and stand next to
+        ##the person that takes the guesses
+        ##take the average of all the guesses
+        ##by everyone else
+        ##you have more statistical power doing this
+        ##than by guessing alone
+##Random forest = a team of decision trees
+
